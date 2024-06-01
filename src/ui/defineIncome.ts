fca0d84ed6c0f795f1c6267dda5f5bd2e3784fa0
DryingCore <lein0rb4g@gmail.com>
2024-06-01 21:27:48 +0000

    Jun 1, 2024, 6:27 PM

--- src/ui/defineIncome.ts
+++ src/ui/defineIncome.ts
@@ -1,8 +1,15 @@
-import { defDate } from "./input.js";
 import { dIC } from "./checkbox.js";
 
 // dI => define income
 export async function dI() {
 	console.log("New income!");
 	defDate().then(dIC);
+}
+
+import { input } from "@inquirer/prompts";
+
+// defDate => define date
+export async function defDate() {
+	const incomeDate = await input({ message: "Income date:" });
+	return incomeDate;
 }
--- src/ui/input.ts
+++
@@ -1,7 +0,0 @@
-import { input } from "@inquirer/prompts";
-
-// defDate => define date
-export async function defDate() {
-	const incomeDate = await input({ message: "Income date:" });
-	return incomeDate;
-}
