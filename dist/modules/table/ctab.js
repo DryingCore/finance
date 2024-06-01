a5a465b6d08f460fd1900d3d8dcfeb9d973f00c7
DryingCore <lein0rb4g@gmail.com>
2024-06-01 03:17:01 +0000

    Jun 1, 2024, 12:17 AM

--- dist/modules/table/ctab.js
+++ dist/modules/table/ctab.js
@@ -6,24 +6,24 @@ export function ctab() {
         chars: {
             top: "-",
             "top-mid": "-",
-            "top-left": "",
-            "top-right": "╗",
-            bottom: "═",
-            "bottom-mid": "╧",
-            "bottom-left": "╚",
-            "bottom-right": "╝",
-            left: "║",
-            "left-mid": "╟",
+            "top-left": "-",
+            "top-right": "|",
+            bottom: "-",
+            "bottom-mid": "-",
+            "bottom-left": "-",
+            "bottom-right": "-",
+            left: "|",
+            "left-mid": "|",
             mid: "─",
-            "mid-mid": "┼",
-            right: "║",
-            "right-mid": "╢",
+            "mid-mid": "|",
+            right: "|",
+            "right-mid": "|",
             middle: "│",
         },
         head: ["Date", "Balance", "Category", "Value", "Type"],
         colWidths: [20, 20, 20, 20, 20],
     });
     // table is an Array, so you can `push`, `unshift`, `splice` and friends
-    table.push(["15/06/2007", "R$ 600"], ["18/02/2008", "R$ 5900"]);
+    table.push(["15/06/2007", "R$ 600", "Bills", "-2.50", "Expense"], ["18/02/2008", "R$ 5900"]);
     console.log(table.toString());
 }
