b8702622a2c4e61cfab73cfd4c559da76908af3e
DryingCore <lein0rb4g@gmail.com>
2024-06-01 02:53:58 +0000

    May 31, 2024, 11:53 PM

---
+++ dist/modules/interface/prompt.js
@@ -0,0 +1,2 @@
+import input from '@inquirer/input';
+const answer = await input({ message: 'Enter your name' });
---
+++ src/modules/interface/prompt.ts
@@ -0,0 +1,3 @@
+import input from '@inquirer/input';
+
+const answer = await input({ message: 'Enter your name' });
