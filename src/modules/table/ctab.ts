dc9aa8c83c52ab69001dd0d9218d8b2fd01636b9
DryingCore <lein0rb4g@gmail.com>
2024-06-01 03:14:33 +0000

    Jun 1, 2024, 12:14 AM

--- src/modules/table/ctab.ts
+++ src/modules/table/ctab.ts
@@ -4,6 +4,24 @@ import Table from "cli-table";
 // ctab => create table
 export function ctab() {
 	const table = new Table({
+		chars: {
+			top: "═",
+			"top-mid": "╤",
+			"top-left": "╔",
+			"top-right": "╗",
+			bottom: "═",
+			"bottom-mid": "╧",
+			"bottom-left": "╚",
+			"bottom-right": "╝",
+			left: "║",
+			"left-mid": "╟",
+			mid: "─",
+			"mid-mid": "┼",
+			right: "║",
+			"right-mid": "╢",
+			middle: "│",
+		},
+
 		head: ["Date", "Balance", "Category", "Value", "Type"],
 		colWidths: [20, 20, 20, 20, 20],
 	});
