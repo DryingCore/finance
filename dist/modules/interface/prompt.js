84ce8fe508db7b0cd6834f25c5c9f003175ecb72
DryingCore <lein0rb4g@gmail.com>
2024-06-01 16:09:40 +0000

    Jun 1, 2024, 1:09 PM

--- dist/modules/interface/prompt.js
+++ dist/modules/interface/prompt.js
@@ -1,7 +1,8 @@
 import input from "@inquirer/input";
+import { dIC } from "./checkbox.js";
 // dI => define income
 export async function dI() {
     console.log("New income!");
     const incomeDate = await input({ message: "Income date:" });
-    // category checkbox
+    dIC();
 }
--- src/modules/interface/checkbox.ts
+++ src/modules/interface/checkbox.ts
@@ -1,2 +1,12 @@
 import { checkbox } from "@inquirer/prompts";
 
+// dIC => define income category
+export async function dIC() {
+	const category = await checkbox({
+		message: "What's the income category?",
+		choices: [
+			{ name: "Payday", value: "Payday" },
+			{ name: "Offshore", value: "a" },
+		],
+	});
+}
