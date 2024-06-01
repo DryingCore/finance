b6996b084f831da983d7960b1b5054932ac05fd7
DryingCore <lein0rb4g@gmail.com>
2024-06-01 16:13:50 +0000

    Jun 1, 2024, 1:13 PM

--- dist/modules/interface/defineIncome.js
+++ dist/modules/interface/defineIncome.js
@@ -1,5 +1,5 @@
-import { dIC } from "./checkbox.js";
 import { defDate } from "./input.js";
+import { dIC } from "./checkbox.js";
 // dI => define income
 export async function dI() {
     console.log("New income!");
