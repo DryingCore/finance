0a95e22f130eab0b3a3dc2d64d2f618d56ad81fa
DryingCore <lein0rb4g@gmail.com>
2024-06-01 21:11:07 +0000

    Jun 1, 2024, 6:11 PM

--- dist/modules/interface/prompt.js
+++
@@ -1,8 +0,0 @@
-import input from "@inquirer/input";
-import { dIC } from "./checkbox.js";
-// dI => define income
-export async function dI() {
-    console.log("New income!");
-    const incomeDate = await input({ message: "Income date:" });
-    dIC();
-}
--- dist/src/index.js
+++
@@ -1,3 +0,0 @@
-import { sOpt } from "./modules/interface/sOpt.js";
-import { vS } from "./modules/logical/verify.js";
-sOpt().then(response => vS(response));
