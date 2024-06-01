213a32e8fa3c2101100b22202bcfd62d46a3b068
DryingCore <lein0rb4g@gmail.com>
2024-06-01 16:23:21 +0000

    Jun 1, 2024, 1:23 PM

--- teste.ts
+++ teste.ts
@@ -1,19 +1,22 @@
-const inquirer = require("inquirer");
-const datePrompt = require("inquirer-datepicker-prompt");
+// selectDate.mjs
 
-inquirer.registerPrompt("datetime", datePrompt);
+import inquirer from 'inquirer';
+import datePrompt from 'inquirer-datepicker-prompt';
+
+// Register the custom prompt
+inquirer.registerPrompt('datetime', datePrompt);
 
 async function selectDate() {
-	const response = await inquirer.prompt([
-		{
-			type: "datetime",
-			name: "date",
-			message: "Please select a date",
-			format: ["Y", "-", "m", "-", "d"],
-		},
-	]);
+    const response = await inquirer.prompt([
+        {
+            type: 'datetime',
+            name: 'date',
+            message: 'Please select a date',
+            format: ['Y', '-', 'm', '-', 'd'],
+        },
+    ]);
 
-	console.log("Selected Date:", response.date);
+    console.log('Selected Date:', response.date);
 }
 
 selectDate();
