c797987365640c5e302a57a2fd11031444e9b918
DryingCore <lein0rb4g@gmail.com>
2024-06-20 19:11:29 +0000

    Jun 20, 2024, 4:11 PM

--- src/modules/income/IncomeBalance.ts
+++ src/modules/income/IncomeBalance.ts
@@ -4,7 +4,7 @@ export async function defBalance() {
 	const userBalance = await prompts({
 		type: "text",
 		name: "balance",
-		message: "What's the income value?",
+		message: "What's the income value? R$",
 	});
 
 	return userBalance;
