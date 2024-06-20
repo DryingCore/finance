21fb81a0b0931b9d44d438a25e62867518dc98cb
DryingCore <lein0rb4g@gmail.com>
2024-06-20 18:43:03 +0000

    Jun 20, 2024, 3:43 PM

--- dist/modules/global/calendar.js
+++
@@ -1,9 +0,0 @@
-// Necessary imports
-import { renderCalendar } from "./calendar/renderCalendar.js";
-import { selectDay } from "./calendar/selectDay.js";
-import { format } from "date-fns"; // importing necessary modules
-export async function promptCalendar() {
-    renderCalendar();
-    const selectedDay = await selectDay();
-    console.log(`You have selected: ${format(selectedDay, "do MMMM yyyy")}`);
-}
