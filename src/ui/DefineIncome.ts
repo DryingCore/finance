a215e5d38024ba6ac8ce249df8a9d48f42e0c130
DryingCore <lein0rb4g@gmail.com>
2024-06-20 19:26:27 +0000

    Jun 20, 2024, 4:26 PM

--- src/ui/DefineIncome.ts
+++ src/ui/DefineIncome.ts
@@ -3,6 +3,8 @@ import { defCategory } from "../modules/income/defCategory.js";
 import { defBalance } from "../modules/income/defBalance.js";
 
 export async function DefineIncome() {
-	const [date, category, balance] = await Promise.all([defDate(), defCategory(), defBalance()]);
+	const date = await defDate();
+	const category = await defCategory();
+	const balance = await defBalance();
 	console.log({ date, category, balance });
 }
